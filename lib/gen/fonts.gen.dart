22090f577086e30bed69ecce66092bd021845693
Asim Siddiqui <asimsiddiqui20172017@gmail.com>
2023-09-25 01:22:20 +0530

    fonts configuration

---
+++ lib/gen/fonts.gen.dart
@@ -0,0 +1,51 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class FontFamily {
+  FontFamily._();
+
+  /// Font family: OpenSans-Bold
+  static const String openSansBold = 'OpenSans-Bold';
+
+  /// Font family: OpenSans-BoldItalic
+  static const String openSansBoldItalic = 'OpenSans-BoldItalic';
+
+  /// Font family: OpenSans-ExtraBold
+  static const String openSansExtraBold = 'OpenSans-ExtraBold';
+
+  /// Font family: OpenSans-ExtraBoldItalic
+  static const String openSansExtraBoldItalic = 'OpenSans-ExtraBoldItalic';
+
+  /// Font family: OpenSans-Italic
+  static const String openSansItalic = 'OpenSans-Italic';
+
+  /// Font family: OpenSans-Light
+  static const String openSansLight = 'OpenSans-Light';
+
+  /// Font family: OpenSans-LightItalic
+  static const String openSansLightItalic = 'OpenSans-LightItalic';
+
+  /// Font family: OpenSans-Medium
+  static const String openSansMedium = 'OpenSans-Medium';
+
+  /// Font family: OpenSans-MediumItalic
+  static const String openSansMediumItalic = 'OpenSans-MediumItalic';
+
+  /// Font family: OpenSans-Regular
+  static const String openSansRegular = 'OpenSans-Regular';
+
+  /// Font family: OpenSans-SemiBold
+  static const String openSansSemiBold = 'OpenSans-SemiBold';
+
+  /// Font family: OpenSans-SemiBoldItalic
+  static const String openSansSemiBoldItalic = 'OpenSans-SemiBoldItalic';
+
+  /// Font family: Questrial-Regular
+  static const String questrialRegular = 'Questrial-Regular';
+}
